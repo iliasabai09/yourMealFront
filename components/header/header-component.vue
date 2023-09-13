08c8828cdf99a1a010bddbc0f8301accaf4b090f
iliasabai123 <ilias.abai.alser@gmail.com>
2023-09-13 22:22:45 +0600

    сделал шапку сайта

---
+++ components/header/header-component.vue
@@ -0,0 +1,83 @@
+<template>
+  <header class="header">
+    <NuxtLink class="header__logo" to="/">
+      <img alt="logo" src="/images/logo.svg">
+    </NuxtLink>
+    <div class="header__content">
+      <div><img alt="burger" src="/images/logo_main.svg"></div>
+      <div class="header__description">
+        <div class="header__text">
+          <span style="color: var(--white)">Только самые</span>
+          <br>
+          <span style="color: var(--orange)">сочные бургеры!</span>
+        </div>
+        <div class="header__delivery">Бесплатная доставка от 599₽</div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+@import "assets/styles/devices";
+
+.header {
+  height: 400px;
+  background-color: var(--yello);
+  box-sizing: border-box;
+  border-radius: 0 0;
+  @media (max-width: $mobile) {
+    height: fit-content;
+  }
+
+  &__logo {
+    margin: 0 auto;
+    padding: 16px 0;
+    width: fit-content;
+    display: block;
+  }
+
+  &__content {
+    display: flex;
+    align-items: center;
+    gap: 24px;
+    margin: 0 auto;
+    width: fit-content;
+    @media (max-width: $mobile) {
+      flex-direction: column-reverse;
+    }
+  }
+
+  &__description {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 70px;
+    @media (max-width: $mobile) {
+      gap: 10px;
+    }
+  }
+
+  &__text {
+    font-weight: 800;
+    font-size: 50px;
+    @media (max-width: $mobile) {
+      font-size: 30px;
+      text-align: center;
+    }
+  }
+
+  &__delivery {
+    color: var(--white);
+    font-weight: 600;
+    font-size: 16px;
+    @media (max-width: $mobile) {
+      text-align: center;
+    }
+  }
+}
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header-component></header-component>
+      <slot />
+    <div>footer</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+
+</style>
